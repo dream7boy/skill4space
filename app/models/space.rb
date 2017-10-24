4e122293fa5a27218dbb77fc80b54fd604f66125
junggunlee <leejunggun7@gmail.com>
2017-10-24 19:22:42 +0900

    make booking model

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,5 +2,6 @@ class Space < ApplicationRecord
   validates :name, presence: true
   validates :daily_price, numericality: { only_integer: true, greater_than: 0 }
 
+  has_many :bookings, dependent: :destroy
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :spaces, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
