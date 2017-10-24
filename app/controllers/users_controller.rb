1dc2c54c69311a93df3b9ac8d764a769f65ff2fb
Paul Lee <paul.lee@cantab.net>
2017-10-24 13:07:43 +0900

    add user mvc with login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  protect_from_forgery
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :update]
+
+  def show
+  end
+
+  def update
+    @user.update(users_params)
+    redirect_to user_path(@user)
+    flash[:notice] = "Your profile was edited"
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:email, :name, :address)
+  end
+
+  def set_user
+    @user = current_user
+  end
+end
