e7c54db5828d003bcfb6a069fd7e8ec27a97cbf0
Paul Lee <paul.lee@cantab.net>
2017-10-25 22:26:51 +0900

    user review mvc created

---
+++ app/controllers/user_reviews_controller.rb
@@ -0,0 +1,25 @@
+class UserReviewsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @user_review = @user.user_reviews.build
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @user_review = @user.user_reviews.build(user_review_params)
+    # @user_review.space = current_space
+
+    if @user_review.save
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_review_params
+    params.require(:user_review).permit(:content, :rating)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025070408) do
+ActiveRecord::Schema.define(version: 20171025115424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,17 @@ ActiveRecord::Schema.define(version: 20171025070408) do
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
+  create_table "user_reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.bigint "space_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["space_id"], name: "index_user_reviews_on_space_id"
+    t.index ["user_id"], name: "index_user_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +97,6 @@ ActiveRecord::Schema.define(version: 20171025070408) do
   add_foreign_key "bookings", "users"
   add_foreign_key "skills", "users"
   add_foreign_key "spaces", "users"
+  add_foreign_key "user_reviews", "spaces"
+  add_foreign_key "user_reviews", "users"
 end
