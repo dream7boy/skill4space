6c461b3a250040756713cbb6e1459a0749cbf54f
Paul Lee <paul.lee@cantab.net>
2017-10-26 17:55:20 +0900

    reviewer linked

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026073648) do
+ActiveRecord::Schema.define(version: 20171026084654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20171026073648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "reviewer_name"
     t.index ["user_id"], name: "index_user_reviews_on_user_id"
   end
 
