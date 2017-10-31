fb276a66f748c42d07c45c58e7cb4241384fd8d5
junggunlee <leejunggun7@gmail.com>
2017-10-31 19:42:31 +0900

    prevent empty message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,20 @@ class MessagesController < ApplicationController
   before_action :set_conversation
 
   def create
-    if @conversation.receipts[-2].receiver_id == current_user.id
-      @receiver = User.find(@conversation.receipts[-1].receiver_id)
+    if params[:body].present?
+      if @conversation.receipts[-2].receiver_id == current_user.id
+        @receiver = User.find(@conversation.receipts[-1].receiver_id)
+      else
+        @receiver = User.find(@conversation.receipts[-2].receiver_id)
+      end
+      send_get_message_email(current_user, @receiver)
+      receipt = current_user.reply_to_conversation(@conversation, params[:body])
+      redirect_to conversation_path(params[:conversation_id])
     else
-      @receiver = User.find(@conversation.receipts[-2].receiver_id)
+      # render "conversations/show"
+      flash[:alert] = "Your message is blank."
+      redirect_back(fallback_location: root_path)
     end
-    send_get_message_email(current_user, @receiver)
-    receipt = current_user.reply_to_conversation(@conversation, params[:body])
-    redirect_to conversation_path(params[:conversation_id])
   end
 
   private
