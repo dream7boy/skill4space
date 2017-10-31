fd8ad9129ffb4f0b67535750f266a0fa069688af
Paul Lee <paul.lee@cantab.net>
2017-10-31 17:38:51 +0900

    validation for space

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,10 @@
 class Space < ApplicationRecord
   validates :name, presence: true
+  validates :photos, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :required_skill, presence: true
   # validates :daily_price, numericality: { only_integer: true, greater_than: 0 }
 
   geocoded_by :address
