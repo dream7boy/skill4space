993162c65bc436681398ecd6e1674399c89e2477
TAKU-YEAH <tky.mfwnb@gmail.com>
2017-10-31 16:02:02 +0900

    Change messages on transactional email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
 
-    mail(to: @user.email, subject: 'Welcome to Skill4space')
+    mail(to: @user.email, subject: 'Welcome to skill4.space')
 
     # @greeting = "Hi"
 
@@ -18,34 +18,34 @@ class UserMailer < ApplicationMailer
   def get_message(sender, receiver)
     @sender = sender
     @receiver = receiver
-    mail(to: @receiver.email, subject: "You've got a message from #{@sender.name}")
+    mail(to: @receiver.email, subject: "You've got a message from #{@sender.name} | skill4.space")
   end
 
   def after_booking_owner(booker, space)
     @booker = booker
     @space = space
     @owner = space.user
-    mail(to: @owner.email, subject: "#{@space.name} has been booked!")
+    mail(to: @owner.email, subject: "#{@space.name} has been booked! | skill4.space")
   end
 
   def after_booking_booker(booker, space)
     @booker = booker
     @space = space
     @owner = space.user
-    mail(to: @booker.email, subject: "Booking Made - #{@space.name}")
+    mail(to: @booker.email, subject: "Thank you for your booking - #{@space.name} | skill4.space")
   end
 
   def accept_booking(booker, space)
     @booker = booker
     @space = space
     @owner = space.user
-    mail(to: @booker.email, subject: "Booking Accepted - #{@space.name}")
+    mail(to: @booker.email, subject: "Your booking is now accepted - #{@space.name} | skill4.space")
   end
 
   def decline_booking(booker, space)
     @booker = booker
     @space = space
     @owner = space.user
-    mail(to: @booker.email, subject: "Booking Declined - #{@space.name}")
+    mail(to: @booker.email, subject: "I'm sorry but your booking is declined - #{@space.name} | skill4.space")
   end
 end
