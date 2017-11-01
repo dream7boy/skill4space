4986dc52b8f271818990c943ec966a581511fa78
Paul Lee <paul.lee@cantab.net>
2017-11-01 18:32:35 +0900

    add description to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031094507) do
+ActiveRecord::Schema.define(version: 20171101093048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,7 +118,7 @@ ActiveRecord::Schema.define(version: 20171031094507) do
     t.date "start_date"
     t.date "end_date"
     t.string "floor_area"
-    t.text "people_capacity"
+    t.string "people_capacity"
     t.time "opening_hours"
     t.time "closing_hours"
     t.boolean "is_barter"
@@ -169,6 +169,7 @@ ActiveRecord::Schema.define(version: 20171031094507) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
